eada3517a73a2e924a2707a0c575ed2b46e77bac
Omar Ramirez <74220217+CarlosOmarRamirezZaragoza@users.noreply.github.com>
2026-01-27 16:37:01 -0600

    Delete lib/secrets.dart
    Create a new api key on https://aistudio.google.com/app/u/1/api-keys or check the local lib/secret.dart

--- lib/secrets.dart
+++
@@ -1,3 +0,0 @@
-// Solo por el demo no usar esto en produccion , usar backend.
-// Para que funcione se tiene que tener activo el billing de la key
-const apiKey = 'AIzaSyBuzkyWg1D96FIxOKewa59hnPgyz6hrvoA';
